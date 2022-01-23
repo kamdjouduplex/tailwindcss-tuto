7ab79eeceb6ddbbd2d5da9baffc565f6969b3a10
Duplex <tony14pro@gmail.com>
2022-01-23 12:11:10 +0100

    lecon 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#06b6d4',
+        secondary: {
+          100: '#9333ea',
+          200: '#2563eb'
+        },
+      },
+      fontSize: {
+        pro: '10rem'
+      }
+    },
   },
   plugins: [],
 }
